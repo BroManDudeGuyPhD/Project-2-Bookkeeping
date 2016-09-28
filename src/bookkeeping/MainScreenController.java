4574a27ad2b76f25155b093c036aa83784219002
aaf8553 <aaf8553@cmpslab15.cacs.louisiana.edu>
2016-09-28 13:59:09 -0500

    Added Start button and team pause button, as well as color changing timer when paused

--- src/bookkeeping/MainScreenController.java
+++ src/bookkeeping/MainScreenController.java
@@ -7,10 +7,16 @@ package bookkeeping;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.paint.Color;
+import javafx.util.Duration;
 
 /**
  *
@@ -18,18 +24,68 @@ import javafx.scene.control.Label;
  */
 public class MainScreenController implements Initializable {
     
+    boolean timerOn = false;
+    boolean timerPaused = true;
+    int seconds = 0;
+    int minutes = 0;
+    
+    EventHandler<ActionEvent> eventHandler = null;
+    javafx.animation.Timeline timer = null;
+    
     @FXML
     private Label label;
+    @FXML
+    private Button button;
+    @FXML
+    private Label timerLabel;
+    @FXML 
+    private Button startButton;
+    
     
     @FXML
     private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
+
+        if (!timerOn) {
+            
+            timer.play(); // Start timer
+            timerOn = true;
+            timerPaused = false;
+            button.setText("Pause");
+            timerLabel.setTextFill(Color.GREEN);
+
+        } else if (timerPaused) {
+
+            timer.play();
+            timerPaused = false;
+             timerLabel.setTextFill(Color.GREEN);
+            button.setText("Pause");
+            
+        } else if (!timerPaused) {
+            
+            timer.pause();
+            timerPaused = true;
+            button.setText("Resume");
+            timerLabel.setTextFill(Color.RED);
+        }
+        
     }
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
+
+            eventHandler = e -> {
+                seconds++;
+                if (seconds == 60) {
+                    minutes++;
+                    seconds = 0;
+                }
+                timerLabel.setText(String.format("%02d:%02d", minutes, seconds));
+            };
+            
+            timer = new Timeline(
+                new KeyFrame(Duration.millis(1000), eventHandler));
+            timer.setCycleCount(180);
+            
+    }      
     
 }
